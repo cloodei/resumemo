9419da126a75d5987eaf27c43ef3176247dc8781
Nguyễn Việt Quốc An <40015342+cloodei@users.noreply.github.com>
2026-02-25 23:50:51 +0700

    is 4 even real

--- api/src/lib/auth.ts
+++ api/src/lib/auth.ts
@@ -7,7 +7,7 @@ import * as schema from "@shared/schemas";
 
 export const auth = betterAuth({
   secret: process.env.BETTER_AUTH_SECRET!,
-  // baseURL: process.env.BETTER_AUTH_URL ?? "http://localhost:8080",
+  baseURL: process.env.BETTER_AUTH_URL ?? "http://localhost:8080",
   // basePath: "/api/auth",
   database: drizzleAdapter(db, {
     provider: "pg",
@@ -25,7 +25,7 @@ export const auth = betterAuth({
     //   secure: process.env.NODE_ENV === "production"
     // }
   },
-  trustedOrigins: [process.env.FRONTEND_URL ?? "http://localhost:5000"],
+  trustedOrigins: [process.env.FRONTEND_URL ?? "http://localhost:5173", "http://localhost:5000", "http://localhost:3000"],
   emailAndPassword: {
     enabled: true,
     disableSignUp: false,
