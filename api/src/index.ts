d197308270e6e2ffbd522520f95b75834614268a
an.nvq <dst.an.nvq@gmail.com>
2026-03-15 00:52:54 +0700

    try go sv

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,8 +1,9 @@
 import { cors } from "@elysiajs/cors";
 import { Elysia } from "elysia";
 
-import * as routes from "./routes";
+
 import { authMiddleware } from "./lib/auth";
+import { pipelineCallbackRoutes, sessionRoutes } from "./routes";
 
 const frontendOrigins = (process.env.FRONTEND_URLS
 	?? process.env.FRONTEND_URL
@@ -22,8 +23,8 @@ const app = new Elysia({ precompile: true })
 		}),
 	)
 	.use(authMiddleware)
-	.use(routes.sessionRoutes)
-	.use(routes.pipelineCallbackRoutes)
+	.use(sessionRoutes)
+	.use(pipelineCallbackRoutes)
 	.listen({ hostname: "0.0.0.0", port: 8080 });
 
 export type API = typeof app;
